661f4dff259cf8644545a796f3c92c9279d338ce
2510631250036-Syafi <2510631250036@student.unsika.ac.id>
2026-05-12 11:54:26 +0700

    Refactor ProjekPerpus.java to improve structure

--- ProjekPerpus.java
+++ ProjekPerpus.java
@@ -95,7 +95,7 @@ public class ProjekPerpus {
         dataBuku[jumlahData++] = new Buku(27, "Akuntansi Dasar", "Ekonomi", 10);
         dataBuku[jumlahData++] = new Buku(28, "Ekonomi Mikro", "Ekonomi", 8);
         dataBuku[jumlahData++] = new Buku(29, "Ekonomi Makro", "Ekonomi", 7);
-        dataBuku[jumlahData++] = new Buku(30, "Kewirausahaan", "Ekonomi", 13);
+        dataBuku[jumlahData++] = new Buku(30, "Kepemimpinan", "Ekonomi", 13);
     }
 
     static void tambahBuku() {
